ab45e23853a5e9963fc8e30f36cedf6470103f18
Jerad Berhow <admin@cosmiccubicle.com>
2021-06-26 11:21:14 -0400

    Delete old directories

--- Scripts/Offline/UnZipAll.ps1
+++ Scripts/Offline/UnZipAll.ps1
@@ -1,11 +1,13 @@
 $folderPath="F:\3D Files"
 
-$Archives = Get-ChildItem $folderPath -recurse
+$Archives = Get-ChildItem $folderPath -recurse -Filter *.zip
 Foreach($Archive in $Archives){
-    if($Archive.VersionInfo.FileName.split(".")[1] -eq "zip")
-    {
-        $Target = $Archive.VersionInfo.FileName.split(".")[0] -ErrorAction SilentlyContinue
-        mkdir $Target -ErrorAction SilentlyContinue
-        Expand-Archive -path $Archive.VersionInfo.FileName -destinationpath $Target -Force -ErrorAction SilentlyContinue
-    }
+    $Target = $Archive.VersionInfo.FileName.split(".")[0]
+    7z.exe e $Archive.VersionInfo.FileName -o"$($Target)" -aoa -y
+    remove-item $Archive.VersionInfo.FileName -Force
 }
+$tdc="C:\a\c\d"
+do {
+  $dirs = gci $tdc -directory -recurse | Where { (gci $_.fullName).count -eq 0 } | select -expandproperty FullName
+  $dirs | Foreach-Object { Remove-Item $_ }
+} while ($dirs.count -gt 0)
