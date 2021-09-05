ba41c51ba81df93757deabfdaf9864b33ce1552e
Jerad Berhow <admin@cosmiccubicle.com>
2021-09-05 05:11:29 -0400

    commit New day

--- Scripts/Offline/UnZipAll.ps1
+++ Scripts/Offline/UnZipAll.ps1
@@ -4,10 +4,17 @@ $Archives = Get-ChildItem $folderPath -recurse -Filter *.zip
 Foreach($Archive in $Archives){
     $Target = $Archive.VersionInfo.FileName.split(".")[0]
     7z.exe e $Archive.VersionInfo.FileName -o"$($Target)" -aoa -y
-    remove-item $Archive.VersionInfo.FileName -Force
 }
-$tdc="C:\a\c\d"
+
+Get-ChildItem -path $folderPath *.png -Recurse | Remove-Item -Force
+Get-ChildItem -path $folderPath *.txt -Recurse | Remove-Item -Force
+Get-ChildItem -path $folderPath *.jpg -Recurse | Remove-Item -Force
+Get-ChildItem -path $folderPath *.zip -Recurse | Remove-Item -Force
+Get-ChildItem -path $folderPath *.html -Recurse | Remove-Item -Force
+Get-ChildItem -path $folderPath *.jpeg -Recurse | Remove-Item -Force
+
 do {
-  $dirs = gci $tdc -directory -recurse | Where { (gci $_.fullName).count -eq 0 } | select -expandproperty FullName
-  $dirs | Foreach-Object { Remove-Item $_ }
+  $dirs = Get-ChildItem $folderPath -directory -recurse | `
+          Where-Object { (Get-ChildItem $_.fullName).count -eq 0 } | `
+          Remove-Item -Force -Recurse
 } while ($dirs.count -gt 0)
